4c70a80b29807c82634f6b745ac46d9c78019932
Adinai Isakova <adinaiisakova@MacBook-Pro-Adinai.local>
2024-02-26 15:44:35 -0600

    attach ec2 to tgrp update again

--- attach_ec2tgrp_module/main.tf
+++ attach_ec2tgrp_module/main.tf
@@ -1,12 +1,12 @@
 #attach ec2 to target group:
-#resource "aws_lb_target_group_attachment" "Test" {
-#target_group_arn = var.target_group_arn
-#target_id = var.instance_id
+resource "aws_lb_target_group_attachment" "Test" {
+target_group_arn = var.target_group_arn
+target_id = var.instance_ids
 
-#}
-
-resource "aws_lb_target_group_attachment" "ec2_attachment" {
-  count            = length(var.instance_ids)
-  target_group_arn = aws_lb_target_group.target_group.arn
-  target_id        = var.instance_ids[count.index]
 }
+
+#resource "aws_lb_target_group_attachment" "ec2_attachment" {
+ # count            = length(var.instance_ids)
+  #target_group_arn = aws_lb_target_group.target_group.arn
+  #target_id        = var.instance_ids[count.index]
+#}
