62f7c3594b82497b6e6d834373e4c62300d8a49e
Adinai Isakova <adinaiisakova@MacBook-Pro-Adinai.local>
2024-02-25 18:54:04 -0600

    target group and attach to ec2-instance

---
+++ tg_module/main.tf
@@ -0,0 +1,20 @@
+resource "aws_target_group" "tg_lb" {
+    name = var.name
+    port = var.port
+    protocol = var.protocol
+    vpc_id = var.vpc_id
+  
+}
+
+
+#attach ec2 to target group:
+resource "aws_lb_target_group_attachment" "instance_to_trgp" {
+target_group_arn = var.target_group_arn
+target_id        = var.instance_id
+
+}
+
+
+
+
+
---
+++ tg_module/variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  
+}
+
+variable "port" {
+  
+}
+
+variable "protocol" {
+  
+}
+ 
+variable "vpc_id" {
+  
+}
+
+variable "target_group_arn" {
+  
+}
+
+variable "instance_id" {
+  
+}
