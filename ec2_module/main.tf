0cc4f8fd22ccf0ba0e1c867468dd2da17fc1580b
Adinai Isakova <adinaiisakova@MacBook-Pro-Adinai.local>
2024-02-25 18:13:31 -0600

    ec2 module built

---
+++ ec2_module/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "instance" {
+  ami           = var.ami
+  key_name = var.key_name
+  instance_type = var.instance_type
+  subnet_id     = var.subnet_id
+  security_groups = var.security_group_id
+  tags = {
+    Name = var.instance_tag
+  }
+user_data = var.user_data
+}
---
+++ ec2_module/variables.tf
@@ -0,0 +1,15 @@
+
+variable "ami" {
+}
+variable "key_name" {
+}
+variable "instance_type"  {
+}
+variable "subnet_id" {
+}
+variable "security_group_id" {
+}
+variable "instance_tag" {
+}
+variable "user_data" {
+}
